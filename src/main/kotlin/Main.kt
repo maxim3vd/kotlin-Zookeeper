7cabb0e21eefcc7019872792af2f1d4c8ab05c53
maxim3andr3 <maxim3.vd@gmail.com>
2024-09-16 14:30:22 +0200

    Add functionality to store user input, process it, and close the program

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println(CAMEL)
+    print("Please enter the number of the habitat you would like to view: ")
+    val number = readln().toInt()
+    println(habitats[number])
+    println(
+        """
+        ---
+        You've reached the end of the program. To check another habitat, please restart the watcher.
+    """.trimIndent()
+    )
 }
