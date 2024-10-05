f5b99f7d30a76479546d8a9f6e3596541d6a0442
maxim3andr3 <maxim3.vd@gmail.com>
2024-10-05 08:10:26 +0200

    Update the program's ending message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,12 @@ fun main() {
     do {
         print("Please enter the number of the habitat you would like to view: ")
         val habitat = readln()
-        
+
         if (habitat != "exit") {
             val number = habitat.toInt()
             println(habitats[number])
         }
     } while (habitat != "exit")
 
-    println(
-        """
-        ---
-        You've reached the end of the program. To check another habitat, please restart the watcher.
-    """.trimIndent()
-    )
+    println("See you later!")
 }
