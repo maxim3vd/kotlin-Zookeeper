1ed512a07d6890563f53884e3a179783ed5938aa
maxim3andr3 <maxim3.vd@gmail.com>
2024-10-05 08:07:18 +0200

    Add functionality to repeat behavior from previous stage and exit program when user enters 'exit'

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,14 @@
 fun main() {
-    print("Please enter the number of the habitat you would like to view: ")
-    val number = readln().toInt()
-    println(habitats[number])
+    do {
+        print("Please enter the number of the habitat you would like to view: ")
+        val habitat = readln()
+        
+        if (habitat != "exit") {
+            val number = habitat.toInt()
+            println(habitats[number])
+        }
+    } while (habitat != "exit")
+
     println(
         """
         ---
